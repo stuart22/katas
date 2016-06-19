25535267c2f4fd7a84af80e957872ad856727854
Stuart Hall <stuart22@gmail.com>
2016-06-18 22:13:50 -0400

    Update

--- kata5/src/kata5/core.clj
+++ kata5/src/kata5/core.clj
@@ -6,14 +6,28 @@
             [clojure.string :as str]
             [clojure.pprint]
             [clojure.java.jdbc :as jdbc]
-            [java-jdbc.ddl :as ddl])
-  (:use [korma.db]
-        [clojure.java.jdbc])
+            [java-jdbc.sql :as sql]
+            [korma.db :as korma])
   (:gen-class))
 
+(declare visitor portnum vis-log)
+
+(let [db-host "localhost"
+      db-port "5432"
+      db-name "kata5db"]
+
+  (def db-spec {:classname "org.postgresql.Driver" ; must be in classpath
+                :subprotocol "postgresql"
+                :subname (str "//" db-host ":" db-port "/" db-name)
+                :user "user"
+                :password "mypass"}))
+(korma/defdb db (korma/postgres db-spec))
 
 
+(defn get-visitors [db-spec]
+  (jdbc/query db-spec ["SELECT ALL name FROM Visitors"]))
 
+(def vis-log (get-visitors db-spec))
 
 
 (defn index-page [username]
@@ -23,35 +37,26 @@
   (do
     (print (str "Username:" username))
     (concat "<form>\n Hello " username
-            "<br> Your name:<br> \n <input type=\"text\" name=arg1>\n</form>"))))
+            "<br> Your name:<br> \n <input type=\"text\" name=arg1>\n</form> <br>" vis-log))))
 
-
-(let [db-host "localhost"
-      db-port (last port)
-      db-name "kata5db"]
-
-  (def db-spec {:classname "org.postgresql.Driver" ; must be in classpath
-                :subprotocol "postgresql"
-                :subname (str "//" db-host ":" db-port "/" db-name)
-                :user "user"
-                :password "mypass"}))
 
 
 (defroutes myapp
-  (GET "/" request-map (do
-                         (clojure.pprint/pprint request-map)
+           (GET "/" request-map (do
+                                  (clojure.pprint/pprint request-map)
+                                  (def visitor (apply str (last (split-at 5 (:query-string request-map)))))
+                                  (jdbc/db-do-commands "postgresql://localhost:5432/kata5db"
+                                                       (jdbc/create-table-ddl :Visitors [[:data :text]]))
+                                  (jdbc/insert! db-spec :Visitors
+                                                {:name visitor})
+                                  (index-page visitor))))
 
-                         (jdbc/db-do-commands "postgresql://localhost:5432/kata5db"
-                                              (jdbc/create-table-ddl :Visitors [[:data :text]]))
-                         (jdbc/insert! db-spec :Visitors
-                                                {:name (apply str (last (split-at 5 (:query-string request-map))))})
-                         (index-page (apply str (last (split-at 5 (:query-string request-map)))) ))))
+
 
 (defn -main [& port]
   (do
     (print (str "Starting server on port " (last port)))
-    (run-server myapp {:port 5432})))
+    (def portnum (last port))
 
-
-
+    (run-server myapp {:port (read-string portnum)})))
 
